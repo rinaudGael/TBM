539e7790e614978fb4c18805f1c11e0175b3b24b
rinaudgael <rinaudgaelrajaonarison@gmail.com>
2026-05-13 20:02:32 +0300

    Ajout du app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import Navbar from './components/Navbar'
+// src/App.jsx
+import GlobalStyles    from './styles/GlobalStyles';
+import Navbar          from './components/Navbar';
+import HeroSection     from './components/HeroSection';
+import MissionsSection from './components/MissonsSection';
+import ProgrammesSection from './components/ProgrammesSection';
+import ParcoursSection from './components/ParcoursSection';
+import ValeursSection  from './components/ValeursSection';
+import ContactSection  from './components/ContactSection';
+import Footer          from './components/Footer';
 
-
-
-export default function App()
-{
+export default function App() {
   return (
     <>
-      <Navbar/>
+      <GlobalStyles />
+      <div className="min-h-screen bg-[#F9F7F2] text-[#2D2D2D]">
+        <Navbar />
+        <HeroSection />
+        <MissionsSection />
+        <ProgrammesSection />
+        <ParcoursSection />
+        <ValeursSection />
+        <ContactSection />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
